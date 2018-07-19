8b5e5795f09934f220df5d52532600969234c71c
dekokun <dekokun@hatena.ne.jp>
2018-07-19 13:40:31 +0900

    freestanding executable on Mac

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn main() -> ! {
+    loop {}
+}
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
