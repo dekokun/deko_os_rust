495a577bd0a4697d39768696ee8136d32ab2a9d2
dekokun <dekokun@hatena.ne.jp>
2018-07-19 21:37:01 +0900

    build kernel

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,13 @@
 
 use core::panic::PanicInfo;
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+#[no_mangle] // don't mangle the name of this function
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop {}
 }
+
 #[panic_implementation]
 #[no_mangle]
 pub fn panic(_info: &PanicInfo) -> ! {
