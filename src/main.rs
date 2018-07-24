1b731f770de1bca06f6fc14d6c320bcd418c604d
dekokun <dekokun@hatena.ne.jp>
2018-07-24 21:40:34 +0900

    split library

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,16 @@
 #![cfg_attr(not(test), no_main)]
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
-#[cfg(test)]
-extern crate std;
-#[cfg(test)]
-extern crate array_init;
-
-
 #[macro_use]
-extern crate lazy_static;
-extern crate spin;
-
-extern crate bootloader_precompiled;
-extern crate volatile;
-extern crate uart_16550;
-extern crate x86_64;
-
-pub unsafe fn exit_qemu() {
-    use x86_64::instructions::port::Port;
-
-    let mut port = Port::<u32>::new(0xf4);
-    port.write(0);
-}
+extern crate deko_os;
 
 use core::panic::PanicInfo;
 
-#[macro_use]
-mod vga_buffer;
-#[macro_use]
-mod serial;
-
-#[no_mangle] // don't mangle the name of this function
+#[cfg(not(test))]
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print!("Hello again deko");
-    println!(", some numbers: {} {}", 42, 1.337);
-    serial_println!("Hello Host{}", "!");
-    unsafe { exit_qemu(); }
+    println!("Hello World{}", "!");
+
     loop {}
 }
 
