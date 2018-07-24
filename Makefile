2f85146920c0cb447069a7418d0bcd8d864245b9
dekokun <dekokun@hatena.ne.jp>
2018-07-24 13:58:54 +0900

    qemu exit on last

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ run:
 	docker-compose run deko_os make -C /tmp/ build
 	qemu-system-x86_64 \
         -drive format=raw,file=$(TARGET) \
+        -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
         -serial mon:stdio
 
 test:
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,14 @@ extern crate spin;
 extern crate bootloader_precompiled;
 extern crate volatile;
 extern crate uart_16550;
+extern crate x86_64;
+
+pub unsafe fn exit_qemu() {
+    use x86_64::instructions::port::Port;
+
+    let mut port = Port::<u32>::new(0xf4);
+    port.write(0);
+}
 
 use core::panic::PanicInfo;
 
@@ -29,6 +37,7 @@ pub extern "C" fn _start() -> ! {
     print!("Hello again deko");
     println!(", some numbers: {} {}", 42, 1.337);
     serial_println!("Hello Host{}", "!");
+    unsafe { exit_qemu(); }
     loop {}
 }
 
