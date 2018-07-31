ae5ffc874adb2c11bc13988a1972827539be8cc0
dekokun <dekokun@hatena.ne.jp>
2018-07-31 20:57:48 +0900

    add double fault by stack overflow test

---
+++ src/bin/test-stackoverflow.rs
@@ -0,0 +1,58 @@
+#![feature(panic_implementation)]
+#![feature(abi_x86_interrupt)]
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+#[macro_use]
+extern crate deko_os;
+
+use core::panic::PanicInfo;
+use deko_os::exit_qemu;
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    deko_os::gdt::init();
+    init_idt();
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+    stack_overflow();
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate x86_64;
+use x86_64::structures::idt::{InterruptDescriptorTable, ExceptionStackFrame};
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault.set_handler_fn(double_fault_handler).set_stack_index(deko_os::gdt::DOUBLE_FAULT_IST_INDEX);;
+        }
+        idt
+    };
+}
+pub fn init_idt() {
+        IDT.load();
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: &mut ExceptionStackFrame, _error_code: u64)
+{
+    serial_println!("ok");
+    unsafe {
+        exit_qemu();
+    }
+}
